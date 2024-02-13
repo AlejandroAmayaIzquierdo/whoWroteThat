71a8ce02ee0d6d8d43f82c7f8cddea84a572cde8
AlejandroAmaya <alejandroamaya@gmail.com>
2024-02-13 17:04:25 +0000

    update db

--- server/kubernetes/UPDATES/1.0.0/update.sql
+++ server/kubernetes/UPDATES/1.0.0/update.sql
@@ -1,1 +1,14 @@
 ﻿INSERT INTO `cron_tasks` (`name`, `schedule`, `is_active`, `lastEnd`) VALUES ('SessionCron', '0 * * * *', 1, '2023-12-03 17:33:43');
+
+
+INSERT INTO `chats` (`id`, `pack`, `difficulty`) VALUES (1, 'initial', 1);
+INSERT INTO `chats` (`id`, `pack`, `difficulty`) VALUES (2, 'initial', 1);
+INSERT INTO `chats` (`id`, `pack`, `difficulty`) VALUES (3, 'iniital', 1);
+INSERT INTO `chats` (`id`, `pack`, `difficulty`) VALUES (4, 'initial', 1);
+
+
+INSERT INTO `messages` (`messageID`, `chatID`, `message`, `isMine`) VALUES (1, 1, 'Haha saw a stripper last night that looked exactly like you but a little hotter', 0);
+INSERT INTO `messages` (`messageID`, `chatID`, `message`, `isMine`) VALUES (2, 2, 'I enjoyed our date but I know it was you who farted', 0);
+INSERT INTO `messages` (`messageID`, `chatID`, `message`, `isMine`) VALUES (3, 3, 'Any last words?', 0);
+INSERT INTO `messages` (`messageID`, `chatID`, `message`, `isMine`) VALUES (4, 4, 'Two roads diverged in a wood, and I, I took the one less travveled by, and tha has made all the difference.', 0);
+INSERT INTO `messages` (`messageID`, `chatID`, `message`, `isMine`) VALUES (5, 4, 'So how do yoi feel about anal now, babe?', 0);
--- server/kubernetes/chats.sql
+++ server/kubernetes/chats.sql
@@ -12,9 +12,10 @@ CREATE TABLE `messages` (
 	`messageID` INT(11) NOT NULL AUTO_INCREMENT,
 	`chatID` INT(11) NOT NULL,
 	`message` VARCHAR(300) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
-	PRIMARY KEY (`messageID`) USING BTREE
+	`isMine` TINYINT(4) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`messageID`) USING BTREE,
+	INDEX `chat` (`chatID`) USING BTREE
 )
 COLLATE='latin1_swedish_ci'
 ENGINE=MyISAM
 ;
-
