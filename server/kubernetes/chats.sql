274d87b807f1ce7bf35fb62d36d160c41b9e6c00
AlejandroAmaya <alejandroamaya@gmail.com>
2024-02-13 12:57:00 +0000

    update db with chats

---
+++ server/kubernetes/chats.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE `chats` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`pack` VARCHAR(50) NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
+	`difficulty` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=MyISAM
+;
+
+CREATE TABLE `messages` (
+	`messageID` INT(11) NOT NULL AUTO_INCREMENT,
+	`chatID` INT(11) NOT NULL,
+	`message` VARCHAR(300) NOT NULL DEFAULT '' COLLATE 'latin1_swedish_ci',
+	PRIMARY KEY (`messageID`) USING BTREE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=MyISAM
+;
+
