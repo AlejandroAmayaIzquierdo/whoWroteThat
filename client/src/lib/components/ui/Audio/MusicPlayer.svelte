c7b39f2870a54c8bdc51eb6a319701c79119621d
Alejandro amaya <alejandroamayaizquierdo@gmail.com>
2024-03-24 21:45:05 +0000

    chore: autoplay audio

--- client/src/lib/components/ui/Audio/MusicPlayer.svelte
+++ client/src/lib/components/ui/Audio/MusicPlayer.svelte
@@ -3,33 +3,39 @@
     import Pause from '@iconify/icons-bi/pause-fill';
     import Icon from "@iconify/svelte";
     import Slider from '../Slider/Slider.svelte';
+	import { onMount } from 'svelte';
 
     let showSlider: boolean = false;
 
-    let audio: HTMLAudioElement; // Declare audio element without initialization
+    let audio: HTMLAudioElement | null = null; // Initialize audio as null
     let isPlaying = false;
 
     let volume: number = 20;
 
     // Function to initialize the audio element
-    const initializeAudio = () => {
+    export const initializeAudio = () => {
         if (!audio) {
             audio = new Audio('/Options-Mario-Kart-Wii.mp3');
             audio.loop = true;
-            audio.volume = 0.2;
+            audio.volume = volume / 100;
+            audio.play();
+            isPlaying = true;
         }
     };
 
     // Function to handle play/pause
     const playPauseAudio = () => {
-        initializeAudio(); // Ensure audio is initialized before playing or pausing
-		if (audio.paused) {
-			audio.play();
-			isPlaying = true;
-		} else {
-			audio.pause();
-			isPlaying = false;
-		}	 	
+        if (!audio) {
+            initializeAudio();
+        }
+
+        if (audio && audio.paused) {
+            audio.play();
+            isPlaying = true;
+        } else if (audio) {
+            audio.pause();
+            isPlaying = false;
+        }	 	
 	}
 
     const updateVolume = (newVolume: number) => {
@@ -38,28 +44,12 @@
             audio.volume = volume / 100; // Update audio volume
         }
     };
-
-    // Automatically play audio when isPlaying changes
-    $: {
-        if (isPlaying) {
-            initializeAudio(); // Ensure audio is initialized before playing
-            if (audio) {
-                audio.play();
-            }
-        } else {
-            if (audio) {
-                audio.pause();
-            }
-        }
-    }
-
-    
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div 
     on:mouseenter={() => {showSlider = true}} 
-    on:mouseleave={() => {showSlider = false}}  
+    on:mouseleave={() => {showSlider = false}}
     class="z-10 absolute left-2 top-2 flex items-center rounded-xl 
     backdrop-blur-xl shadow-xl bg-white gap-4 px-2 sm:px-4 py-1 sm:py-2 sm:text-2xl min-w-fit"
     style="transition: transform 0.3s ease;"
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,11 +1,32 @@
-<script>
+<script lang="ts">
     import MusicPlayer from "$lib/components/ui/Audio/MusicPlayer.svelte";
+	import { onMount } from "svelte";
     import "../app.pcss";
+
+
+    let player: any;
+    let userInteracted = false; // Flag to track user interaction
+
+    // Function to handle user interaction
+    const handleUserInteraction = () => {
+        if (!userInteracted) {
+            player?.initializeAudio();
+            userInteracted = true; // Set flag to true to indicate user interaction
+        }
+    };
+
+    onMount(() => {
+        // Listen for user interaction event
+        window.addEventListener('click', handleUserInteraction, { once: true });
+        window.addEventListener('keydown', handleUserInteraction, { once: true });
+    });
 </script>
 
 
 
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="h-full w-full absolute top-0 left-0 mx-auto">
-    <MusicPlayer />
+    <MusicPlayer bind:this={player} />
     <slot/>
 </div>
